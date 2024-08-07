288ce4bf79b67fe9c046084c8b16c6de087cf4a6
xavi1921 <79384377+xavi1921@users.noreply.github.com>
2024-08-07 08:36:16 -0500

    update auth

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -1,6 +1,8 @@
 import User from '../models/user'
 import jwt from 'jsonwebtoken'
 import config from '../config'
+import rol from '../models/rol';
+import user from '../models/user';
 
 export const signup = async (req,res)=>{
 
@@ -12,8 +14,18 @@ export const signup = async (req,res)=>{
         password: await User.encryptPassword(password)
     })
 
-    const saveUser=await newUser.save();
+    //valida propiedad rol al momento de crear usuario
+    if(roles){
+        const foundRol = await rol.find({name:{$in: roles}})
+        //guarda un arreglo con los ID del rol en el objeto usuario
+        newUser.roles=foundRol.map(role=> role._id)
+    }else{
+        const role=await rol.findOne({name: "user"})
+        newUser.roles=[role._id]
+    }
 
+    const saveUser=await newUser.save();
+    console.log(saveUser)
     const token=jwt.sign({id:saveUser._id},config.SECRET,{
         expiresIn:86400 //24 horas
     })
@@ -22,5 +34,14 @@ export const signup = async (req,res)=>{
 }
 
 export const signin = async (req,res)=>{
-    res.json('signin')
+    const userFound=await user.findOne({email:req.body.email}).populate("roles")
+    
+    //valida la autenticacion con el correo
+    if(!userFound) return res.status(400).json({message:"User not found"})
+    
+    //valida la contraseña
+    await user.comparePassword(req.body.password)
+    console.log(userFound)
+
+    res.json({token:''})
 }
